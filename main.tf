3dd6ee2eb760fdab011a3c69b16b7992036c7779
ccarbo <carlos.carboderico@telefonica.com>
2020-01-22 10:40:19 +0100

    All data first day

--- main.tf
+++ main.tf
@@ -3,9 +3,17 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "~> 1.40"
 }
-
+#Declare variable local to call it will be local.prefix. 
+locals {
+  prefix = "p3"
+}
 #Create a resource group
 resource "azurerm_resource_group" "p1" {
-  name     = "practica1"
+  name = var.resource_group
   location = "NorthEurope"
 }
+#Output console when apply, when we want to concatenate string + variable :: str '${name_variable}string'
+output "resoruces_group_name" {
+  value = azurerm_resource_group.p1.location
+}
+
